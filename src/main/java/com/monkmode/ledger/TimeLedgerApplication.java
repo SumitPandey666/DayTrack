63054ce9c66040e9bd2802d31d48d9b5d754bbfe
sumit <1920sumitpandey@gmail.com>
2026-03-13 16:41:59 +0530

    initial commit

---
+++ src/main/java/com/monkmode/ledger/TimeLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.monkmode.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeLedgerApplication.class, args);
+	}
+
+}
